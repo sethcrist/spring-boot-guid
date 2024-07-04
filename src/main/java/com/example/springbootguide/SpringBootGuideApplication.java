24d82fe09e1da1f897ce090532ac01b90e9de88a
Seth Crist <sethcrist50@gmail.com>
2024-07-04 13:43:29 -0500

    first push

---
+++ src/main/java/com/example/springbootguide/SpringBootGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGuideApplication.class, args);
+    }
+
+}
